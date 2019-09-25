051928d604283539f973d69c7d0b96ebea682ba8
Soraia <soraia@Soraias-iMac.local>
2019-09-25 17:24:16 -0400

    update

--- src/components/AddReceipt.js
+++ src/components/AddReceipt.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react'
-=======
 import React from 'react'
->>>>>>> 03de89f53fe7d3f62d87a1a88c66be9d4dfa92aa
 import { connect } from 'react-redux'
 import { Field, Formik } from 'formik';
 import styled from 'styled-components';
 import { postReceipt } from '../utils/actions'
 
-<<<<<<< HEAD
-function AddReceipt({ status, newReceipt }) {
-=======
 
 const ReceiptContainer = styled.div`
   width: 66vw;
@@ -32,7 +25,6 @@ const DetailsBox= styled.div`
   flex-direction: column;
   margin: 10px 0;
   padding: 20px;
->>>>>>> 03de89f53fe7d3f62d87a1a88c66be9d4dfa92aa
 
   const [receipts, setReceipts] = useState([])
   useEffect(() => {
@@ -47,12 +39,6 @@ const DetailsBox= styled.div`
 
   }
 
-<<<<<<< HEAD
-  const handleSubmit = event => {
-    event.preventDefault();
-    postReceipt(newReceipt)
-  }
-=======
 function AddReceipt({ postReceipt }) {
   // console.log(postReceipt)
   // const [receipts, setReceipts] = useState([])
@@ -63,7 +49,6 @@ function AddReceipt({ postReceipt }) {
   //   e.preventDefault();
   //   postReceipt.addNewReceipt(receipts)
   // }
->>>>>>> 03de89f53fe7d3f62d87a1a88c66be9d4dfa92aa
 
   return (
     <ReceiptContainer>
