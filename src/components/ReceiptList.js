561a346d179700ce47b356dc8452bb00e41d7412
Soraia <soraia@Soraias-iMac.local>
2019-09-26 13:32:49 -0400

    added styling to css and divs to receiptlist

--- src/components/ReceiptList.js
+++ src/components/ReceiptList.js
@@ -1,16 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import { axiosWithAuth } from '../utils/axiosWithAuth.js'
+import { Field, Formik } from 'formik';
 import { connect } from 'react-redux'
+import index from '../index.css'
 
-
-// import AddReceipt from './AddReceipt'
 import ReceiptCard from './ReceiptCard'
 import { getReceipts } from '../utils/actions'
 import AddReceipt from './AddReceipt';
 
-function ReceiptList({userId}) {
-    // NOTE: The value given to setState() must be of the same type as your value is expected to be
+function ReceiptList({userId, getReceipts}) {
+    
     const [users, setUsers] = useState([]);
     const [query, setQuery] = useState("");
     useEffect(() => {
@@ -22,40 +20,43 @@ function ReceiptList({userId}) {
     };
   
     return (
-      <div className="search-box">
-        <form className="search">
-          <input
-            type="text"
-            onChange={handleInputChange}
-            value={query}
-            name="name"
-            tabIndex="0"
-            className="prompt search-name"
-            placeholder="search by name"
-            autoComplete="off"
-          />
-        </form>
-        <form>
-        <div className="receipt">
-          {users.map(receipt => {
-            return (
-              <div className="receipt-list " key={receipt.userId}>
-                <ReceiptCard 
+        <div className="receipts">
+          <div className="search-box">
+            <form className="search">
+              <input
+                type="text"
+                onChange={handleInputChange}
+                value={query}
+                name="name"
+                tabIndex="0"
+                className="prompt search-name"
+                placeholder="search by name"
+                autoComplete="off"
+              />
+            </form>
+          <form>
+            <div className="receipt">
+              {users.map(receipt => {
+                return (
+                  <div className="receipt-list " key={receipt.userId}>
+                    <ReceiptCard 
                         key={receipt.id}
                         retailer={receipt.retailer}
                         category={receipt.category}
                         date={receipt.date}
                         amount={receipt.amount}
                     /> 
-              </div>
-            );
-          })}
+                  </div>
+                );
+              })}
+            </div>
+          </form>
+          </div>
+            
+          <div className='add-receipt'>
+            <AddReceipt />
+          </div>
         </div>
-        </form>
-        <div className='add-receipt'>
-          <AddReceipt />
-        </div>
-      </div>
     );
   }
 
@@ -73,46 +74,4 @@ function ReceiptList({userId}) {
 
   
 
-
-//     function ReceiptList(props) {
-
-//     const [receipts, setReceipts ] = useState({
-//         retailer: 'Exxon',
-//         category: 'Gas'
-//     });
-
-//     useEffect(() => {
-//       axios
-//       .get(`https://lambda-receipt-tracker.herokuapp.com/api/receipts/users`)
-//       .then(response => {
-//         const receiptInfo = response.data;
-//         console.log(receiptInfo);
-//         setReceipts(receiptInfo);
-//       })
-//       .catch(error => {
-//         console.log("The data was not returned", error);
-//       });
-//   }, []);
-    
-
-//     return (
-//         <div>
-//             {receipts.map(receipt => {
-//                 return (
-//                     <div>
-//                         <AddReceipt />
-//                         <ReceiptCard 
-//                             key={receipt.id}
-//                             retailer={receipt.retailer}
-//                             category={receipt.category}
-//                             date={receipt.date}
-//                             amount={receipt.amount}
-//                     /> 
-//                    </div>
-//                 )
-//             })} 
-            
-//         </div>
-//     )
-// }
 
